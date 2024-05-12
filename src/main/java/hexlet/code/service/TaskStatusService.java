76baa67c8e75723eb486d82d51dd71dfbe305b5d
Borychev Andrey <116582046+d1z3d@users.noreply.github.com>
2024-05-12 20:43:18 +0300

    Update TaskStatusService.java

--- src/main/java/hexlet/code/service/TaskStatusService.java
+++ src/main/java/hexlet/code/service/TaskStatusService.java
@@ -3,7 +3,6 @@ package hexlet.code.service;
 import hexlet.code.dto.taskstatus.TaskStatusCreateDTO;
 import hexlet.code.dto.taskstatus.TaskStatusDTO;
 import hexlet.code.dto.taskstatus.TaskStatusUpdateDTO;
-import hexlet.code.exception.ForbiddenException;
 import hexlet.code.exception.ResourceConflictException;
 import hexlet.code.exception.ResourceNotFoundException;
 import hexlet.code.mapper.TaskStatusMapper;
@@ -36,9 +35,6 @@ public class TaskStatusService {
     }
 
     public TaskStatusDTO create(TaskStatusCreateDTO dto) {
-        //if (userUtils.getCurrentUser() == null) {
-        //    throw new ForbiddenException("You have to login to create task status");
-        //}
         var isTaskStatusExist = taskStatusRepository.existsBySlug(dto.getSlug());
         if (isTaskStatusExist) {
             throw new ResourceConflictException("Slug with name " + dto.getSlug() + " already exist");
@@ -49,9 +45,6 @@ public class TaskStatusService {
     }
 
     public TaskStatusDTO update(Long id, TaskStatusUpdateDTO dto) {
-        //if (userUtils.getCurrentUser() == null) {
-        //    throw new ForbiddenException("You have to login to update task status");
-        //}
         var taskStatus = taskStatusRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Task status with id " + id + " not found"));
         taskStatusMapper.update(dto, taskStatus);
@@ -60,9 +53,6 @@ public class TaskStatusService {
     }
 
     public void delete(Long id) {
-        //if (userUtils.getCurrentUser() == null) {
-        //    throw new ForbiddenException("You have to login to delete task status");
-        //}
         var taskStatus = taskStatusRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Task status with id " + id + " not found"));
         taskStatusRepository.delete(taskStatus);
